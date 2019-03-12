132f4f4ddb28a290e5bab5c90311abfce8bb3a8f
weijin96 <weijin_tan@mymail.sutd.edu.sg>
2019-03-13 00:35:46 +0800

    Updated whaleapi

--- whaleapi/whaleapi/settings.py
+++ whaleapi/whaleapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from whaleapi.settings_secret import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'secret key'
+SECRET_KEY = secretKey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -154,4 +155,4 @@ STATICFILES_DIRS = [
 ]
 
 
-BEARER_TOKEN = {API Key}
+BEARER_TOKEN = APIKey
